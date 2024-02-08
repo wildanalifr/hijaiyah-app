7caf64b50ab85bde19ffd07aee60f0f57c47bd38
Wildan <wildanalifrio@gmail.com>
2024-02-08 21:05:25 +0700

    new repository

---
+++ hijaiyah-app/GetHijaiyaHuruf.swift
@@ -0,0 +1,73 @@
+//
+//  GetHijaiyaHuruf.swift
+//  hijaiyah-app
+//
+//  Created by Wildan on 08/02/24.
+//
+
+import Foundation
+
+func getHijaiyaHuruf(_ huruf: String) -> String {
+    switch huruf {
+    case "ا":
+        return "ا"
+    case "ب":
+        return "ب"
+    case "ت":
+        return "ت"
+    case "ث":
+        return "ث"
+    case "ج":
+        return "ج"
+    case "ح":
+        return "ح"
+    case "خ":
+        return "خ"
+    case "د":
+        return "د"
+    case "ذ":
+        return "ذ"
+    case "ر":
+        return "ر"
+    case "ز":
+        return "ز"
+    case "س":
+        return "س"
+    case "ش":
+        return "ش"
+    case "ص":
+        return "ص"
+    case "ض":
+        return "ض"
+    case "ط":
+        return "ط"
+    case "ظ":
+        return "ظ"
+    case "ع":
+        return "ع"
+    case "غ":
+        return "غ"
+    case "ف":
+        return "ف"
+    case "ق":
+        return "ق"
+    case "ك":
+        return "ك"
+    case "ل":
+        return "ل"
+    case "م":
+        return "م"
+    case "ن":
+        return "ن"
+    case "ه":
+        return "ه"
+    case "و":
+        return "و"
+    case "ء":
+        return "ء"
+    case "ي":
+        return "ي"
+    default:
+        return ""
+    }
+}
--- hijaiyah-app/ViewController.swift
+++ hijaiyah-app/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
+    @IBAction func clickButton(_ sender: UIButton) {
+        let currentTitle = sender.titleLabel?.text ?? ""
+        playSound(getHijaiyaHuruf(currentTitle))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
     }
-
-
+    
+    func playSound(_ huruf:String){
+        print("Huruf: \(huruf)")
+        guard let url = Bundle.main.url(forResource: huruf, withExtension: "mp3", subdirectory: "Sound") else {
+            print("error")
+                   return
+               }
+               do {
+                   player = try AVAudioPlayer(contentsOf: url)
+                   player?.play()
+               } catch let error {
+                   print("Error playing sound: \(error.localizedDescription)")
+               }
+    }
 }
 
